5b3b414429eb9c27e6916248c28ab45cc835e14e
Thunder-Bots <109260295+Thunder-Bots@users.noreply.github.com>
2022-08-03 19:41:43 +0530

    Update creator.py

--- messages/creator.py
+++ messages/creator.py
@@ -2,13 +2,13 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def start_msg(name):
-    msg = f"""*Hey {es(name,version=2)}* ✋✋ *welcome to Jiosaavn downloader bot* ⚡⚡\n
-    _Just send me a jiosaavn song or album link I will send you the audio_\n
-made by @phantom2152 😈😈"""
+    msg = f"""*Hey {es(name,version=2)}* \nI am Jiosaavn downloader bot.I can download music for you from jio saavn\n
+    Just send me a jiosaavn song or album link I will send you the audio file.\n
+Owned by @Best_Movies_Everr"""
     return msg
 
 
 def help_msg():
-    help = """ℹ️⁉⁉ *help*\n
-*just send me a jiosaavn song,album or playlist link, I will send you the audio with lyrics*⚡⚡"""
+    help = """*How to Use me*\n
+*Just send me a jiosaavn song,album or playlist link, I will send you the audio with lyrics*\n\nSee This Video for Help - \n\nContact - @Movies_Thunder_chat_group"""
     return help
